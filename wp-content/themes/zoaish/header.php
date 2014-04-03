a5ecf6fa12d2d0a44045568b6f860c724b9d06b3
nongbreesh <breesh.comsci@gmail.com>
2014-04-03 11:31:06 +0700

    Merge origin/master
    Conflicts:
    	application/views/campaignEdit.php

--- wp-content/themes/zoaish/header.php
+++ wp-content/themes/zoaish/header.php
@@ -33,13 +33,14 @@
     <body <?php body_class(); ?>>
         <div id="page" class="hfeed site">
             <div id="navbar" class="navbar">
+                <?php //get_search_form(); ?>
                 <nav id="site-navigation" class="navigation main-navigation" role="navigation">
-                    <h3 class="menu-toggle"><?php _e('Menu', 'zoaish'); ?></h3>
-                    <a class="screen-reader-text skip-link" href="#content" title="<?php esc_attr_e('Skip to content', 'twentythirteen'); ?>"><?php _e('Skip to content', 'twentythirteen'); ?></a>
+                    <img class="logo"  src="<?php echo get_template_directory_uri(); ?>/images/logo_example.png" />
+
                     <?php wp_nav_menu(array('theme_location' => 'primary', 'menu_class' => 'nav-menu')); ?>
-                    <?php //get_search_form(); ?>
                 </nav><!-- #site-navigation -->
             </div><!-- #navbar -->
+
             <header id="masthead" class="site-header" role="banner">
                 <a class="home-link" href="<?php echo esc_url(home_url('/')); ?>" title="<?php echo esc_attr(get_bloginfo('name', 'display')); ?>" rel="home">
                     <h1 class="site-title"><?php bloginfo('name'); ?></h1>
